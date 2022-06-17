9c34e86d7176f16edb6598160f25e9926e0b6beb
ERUDITE-Labhesh <labhesh.l@consultadd.com>
2022-06-17 15:02:14 -0400

    Added the new function greeting

--- SourceCode.py
+++ SourceCode.py
@@ -1,5 +1,13 @@
+name = "Sam"
+
 def main():
   print("This iis the Source Code Python File")
   print("This is the Main Function") 
  
 main()
+
+def greeting(name):
+  print(f"Hello {name}, Welcome to Consultadd Training")
+
+
+greeting(name)
